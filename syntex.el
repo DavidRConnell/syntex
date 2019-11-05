b20a2ca1acaefdfbde9eafdb812b531f75c9bfc4
David R Connell <davidconnell12@gmail.com>
2019-11-05 10:46:42 -0600

    Add: Basic snippets

--- syntex.el
+++ syntex.el
@@ -4,6 +4,25 @@
 ;;
 ;;; Code:
 
+(defun syntex-insert-emph ()
+  "Insert emph snippet."
+  (interactive)
+  (insert "\\emph{}"))
+
+(defun syntex-insert-bold ()
+  "Insert bold snippet."
+  (interactive)
+  (insert "\\textbf{}"))
+
+(defun syntex-insert-italic ()
+  "Insert bold snippet."
+  (interactive)
+  (insert "\\textit{}"))
+
+(defun syntex-insert-label ()
+  "Insert bold snippet."
+  (interactive)
+  (insert "\\label{}"))
 
 (provide 'syntex)
 ;;; syntex.el ends here
